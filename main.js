6c336bcaae9b313fb5096bdbe1c74b197d3149be
DESKTOP-6P02NRP\Damian Garcia <damian95.gs@gmail.com>
2018-04-14 20:31:57 -0500

    Hacer las cosas como deberian de ser

--- main.js
+++ main.js
@@ -1,209 +1,209 @@
-require('dotenv').config()
-const PORT = process.env.PORT
-const ID = process.env.ID
-const PROCESSES = process.env.PROCESSES
+require('dotenv').config();
+const PORT = process.env.PORT;
+const ID = process.env.ID;
+const PROCESSES = process.env.PROCESSES;
 
 const {
   app,
   BrowserWindow,
   dialog
-} = require('electron')
-const path = require('path')
-const url = require('url')
-const fs = require('fs')
+} = require('electron');
+const path = require('path');
+const url = require('url');
+const fs = require('fs');
 
-const net = require('net')
-let server
+const net = require('net');
+let server;
 const clients = [
   process.env.CLIENT1,
   process.env.CLIENT2,
-  // process.env.CLIENT3,
-]
-let coso = 0
+  process.env.CLIENT3
+];
+let coso = 0;
 
-const ip = require('ip')
-let vector = [0, 0, 0, 0]
-let queue = []
-let aks = 0
+const ip = require('ip');
+let vector = [0, 0, 0, 0];
+let queue = [];
+let aks = 0;
 
-console.log(ip.address())
+// console.log(ip.address())
 
-let win
+let win;
 
 const mergeVectors = (timestamp) => {
-  vector[ID]++
-  vector = vector.map((v, i) => v > timestamp[i] ? v : timestamp[i])
-}
+  vector[ID]++;
+  vector = vector.map((v, i) => v > timestamp[i] ? v : timestamp[i]);
+};
 
 const addToQueue = (request) => {
-  mergeVectors(request.timestamp)
-  queue.push(request)
+  mergeVectors(request.timestamp);
+  queue.push(request);
   queue = queue.sort((a, b) => {
-    const as = a.timestamp.reduce((ac, v) => ac + v, 0)
-    const bs = b.timestamp.reduce((ac, v) => ac + v, 0)
-    return as !== bs ? as > bs : a.from > b.from
-  })
-}
+    const as = a.timestamp.reduce((ac, v) => ac + v, 0);
+    const bs = b.timestamp.reduce((ac, v) => ac + v, 0);
+    return as !== bs ? as > bs : a.from > b.from;
+  });
+};
 
 const unlock = () => {
-  state.event.sender.send('unlock')
-}
+  state.event.sender.send('unlock');
+};
 
 const checkForChanges = () => {
   if (aks === 0) {
     if (queue[0] && queue[0].from === ID) {
       // Write to file
-      const tmp = state.text.split('')
+      const tmp = state.text.split('');
       tmp.splice(
         queue[0].position, queue[0].action === 'added' ? 0 : 1,
         queue[0].action === 'added' ? queue[0].letter : undefined
-      )
+      );
       setState({
         text: tmp.join(''),
         from: queue[0].from,
         position: queue[0].position,
-        action: queue[0].action,
-      })
+        action: queue[0].action
+      });
       const free = {
         type: 'FRE',
         from: ID,
         letter: queue[0].letter, // from state
         position: queue[0].position, // from state
         action: queue[0].action,
-        timestamp: vector,
-      }
-      let answers = 0
-      queue.shift()
+        timestamp: vector
+      };
+      let answers = 0;
+      queue.shift();
       clients.map((ip) => {
-        const client = new net.Socket()
+        const client = new net.Socket();
         client.connect(PORT, ip, () => {
           client.write(JSON.stringify(free), () => {
             answers++;
-            if(answers === process.env.PROCESSES - 1){
-              unlock()
+            if (answers === process.env.PROCESSES - 1) {
+              unlock();
             }
-          })
-        })
-      })
+          });
+        });
+      });
     }
   }
-}
+};
 
 let state = {
   text: '',
   id: ID,
   from: ID,
   fileName: `Leyendo de archivo: ${process.env.FILE}`,
-  filePath: path.join(__dirname, process.env.FILE),
-}
+  filePath: path.join(__dirname, process.env.FILE)
+};
 
 const createWindow = () => {
   win = new BrowserWindow({
     width: 800,
-    height: 600,
-  })
+    height: 600
+  });
 
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
-    slashes: true,
-  }))
+    slashes: true
+  }));
 
-  win.webContents.openDevTools()
+  win.webContents.openDevTools();
   let preventClose = true;
 
   win.on('close', (e) => {
     if (preventClose) {
-      e.preventDefault()
+      e.preventDefault();
       dialog.showMessageBox({
         type: 'question',
-        buttons: ['No', 'Yes',],
+        buttons: ['No', 'Yes' ],
         title: 'You\'re leaving the app!',
         message: 'Do you want to save your changes?'
       }, function (response) {
         preventClose = false;
-        win.close ();
+        win.close();
         if (response === 1) {
-          fs.writeFileSync(state.filePath,state.text, {encoding:'utf8'})
+          fs.writeFileSync(state.filePath, state.text, {encoding: 'utf8'});
         }
-      })
+      });
     }
-  })
+  });
 
   server = net.createServer((socket) => {
     socket.on('data', (msg) => {
-      const message = msg.toString()
-      const msgObj = JSON.parse(message)
-      const indice = coso++
+      const message = msg.toString();
+      const msgObj = JSON.parse(message);
+      const indice = coso++;
       switch (msgObj.type) {
         case 'ACK':
           if (msgObj.from === ID) {
-            aks = (aks + 1) % (PROCESSES - 1)
-            mergeVectors(msgObj.timestamp)
-            checkForChanges()
+            aks = (aks + 1) % (PROCESSES - 1);
+            mergeVectors(msgObj.timestamp);
+            checkForChanges();
           }
-          break
+          break;
         case 'REQ':
-          addToQueue(msgObj)
+          addToQueue(msgObj);
           const ack = {
             type: 'ACK',
             from: msgObj.from,
-            timestamp: vector,
-          }
+            timestamp: vector
+          };
           clients.map((ip) => {
-            const client = new net.Socket()
+            const client = new net.Socket();
             client.connect(PORT, ip, () => {
-              client.write(JSON.stringify(ack))
-            })
-          })
-          break
+              client.write(JSON.stringify(ack));
+            });
+          });
+          break;
         case 'FRE':
-          console.log(msgObj, queue)
+          // console.log(msgObj, queue)
           // Write to file
-          const tmp = state.text.split('')
+          const tmp = state.text.split('');
           tmp.splice(
             msgObj.position, msgObj.action === 'added' ? 0 : 1,
             msgObj.action === 'added' ? msgObj.letter : undefined
-          )
+          );
           setState({
             text: tmp.join(''),
             from: msgObj.from,
             position: msgObj.position,
-            action: msgObj.action,
-          })
-          const actual = queue.findIndex(x => x.from === msgObj.from && x.position === msgObj.position && x.letter === msgObj.letter && x.action === msgObj.action)
-          queue.splice(actual, 1)
-          mergeVectors(msgObj.timestamp)
-          checkForChanges()
-          break
+            action: msgObj.action
+          });
+          const actual = queue.findIndex(x => x.from === msgObj.from && x.position === msgObj.position && x.letter === msgObj.letter && x.action === msgObj.action);
+          queue.splice(actual, 1);
+          mergeVectors(msgObj.timestamp);
+          checkForChanges();
+          break;
       }
-      socket.destroy()
-    })
-  })
+      socket.destroy();
+    });
+  });
 
-  server.listen(PORT)
-}
+  server.listen(PORT);
+};
 
 const setState = (newState) => {
   for (let key in newState) {
-    state[key] = newState[key]
+    state[key] = newState[key];
   }
-  state.event.sender.send('stateChange', state)
-}
+  state.event.sender.send('stateChange', state);
+};
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
-  app.quit()
-})
+  app.quit();
+});
 
 app.on('activate', () => {
   if (win === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
-const ipc = require('electron').ipcMain
+const ipc = require('electron').ipcMain;
 
 const sendMessages = (data) => {
   const request = {
@@ -212,44 +212,44 @@ const sendMessages = (data) => {
     from: ID,
     position: data.pos,
     letter: data.key,
-    action: data.action,
-  }
-  addToQueue(request)
+    action: data.action
+  };
+  addToQueue(request);
   clients.map((ip) => {
-    const client = new net.Socket()
+    const client = new net.Socket();
     client.connect(PORT, ip, () => {
-      client.write(JSON.stringify(request))
-    })
-  })
-}
+      client.write(JSON.stringify(request));
+    });
+  });
+};
 
 ipc.on('documentReady', (event, data) => {
   setState({
     text: String(fs.readFileSync(state.filePath)).replace(/\r?/g, ''),
     event
-  })
-})
+  });
+});
 
 ipc.on('invokeAction', (event, data) => {
-  let diff = getChange(data.text, state.text, data.cursorPosition)
-  sendMessages(diff)
-})
+  let diff = getChange(data.text, state.text, data.cursorPosition);
+  sendMessages(diff);
+});
 
 getChange = (newData, oldData, charPos) => {
-  let data = {}
+  let data = {};
   if (newData.length > oldData.length) {
-    data.key = newData.charAt(charPos - 1)
-    data.pos = charPos - 1
-    data.action = 'added'
+    data.key = newData.charAt(charPos - 1);
+    data.pos = charPos - 1;
+    data.action = 'added';
   } else {
-    data.key = oldData.charAt(charPos)
-    data.pos = charPos
-    data.action = 'deleted'
+    data.key = oldData.charAt(charPos);
+    data.pos = charPos;
+    data.action = 'deleted';
   }
 
-  if(data.key == '\r'){
+  if (data.key == '\r') {
     data.key = '\n';
   }
 
-  return data ;
-}
+  return data;
+};
